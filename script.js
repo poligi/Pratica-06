6ca908d8ad39da2eb24a85d97c407b1f89ec8f95
Giovana Pollo <pollogiih2@gmail.comm>
2024-08-19 18:35:27 -0300

    Praticando JS

---
+++ script.js
@@ -0,0 +1,12 @@
+//questtão 1
+let minhaIdade = 20;
+
+console.log ('Minha idade é ' + minhaIdade + ' anos')
+
+//questão 2
+let x = 3
+let y = 5
+let z = 8
+
+let soma = (x + y +z)
+console.log (soma)
